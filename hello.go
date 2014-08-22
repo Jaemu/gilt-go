c6cadd36b5223da660a83a279be60de53646e85b
Jill <jillian.munson@stonybrook.edu>
2014-08-22 11:19:41 -0400

    gofmt

--- hello.go
+++ hello.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 var alice = Person{"Alice", Female}
 var bob = Person{"Bob", Unspecified}
 
+type SexT int
 
 //iota!
 //get incremented each time it's used
-const(
-	Unspecified = iota
+const (
+	Unspecified SexT = iota
 	//iota is then implied
+	//we can also do math e.g. iota * 2
 	Male
 	Female
 )
@@ -20,16 +23,16 @@ const(
 //Go declares visibility
 //package-internal = lowercase
 //package-external = uppercase
-type Person struct{
+type Person struct {
 	Name string
-	Sex int
-} 
+	Sex  SexT
+}
 
 func main() {
 	//shortand for type inference
 	//longhand for type declaration
-	//% gives a struct, 
-	//v is name of fields 
+	//% gives a struct,
+	//v is name of fields
 	fmt.Printf("%+v \n", alice)
 
 }
