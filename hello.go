b413151d9bac204132679d45fda349d44856765d
Jill <jillian.munson@stonybrook.edu>
2014-08-22 11:07:22 -0400

    iota

--- hello.go
+++ hello.go
@@ -4,18 +4,32 @@ import (
 	"fmt"
 )
 
+var alice = Person{"Alice", Female}
+var bob = Person{"Bob", Unspecified}
+
+
+//iota!
+//get incremented each time it's used
+const(
+	Unspecified = iota
+	//iota is then implied
+	Male
+	Female
+)
+
 //Go declares visibility
 //package-internal = lowercase
 //package-external = uppercase
 type Person struct{
 	Name string
+	Sex int
 } 
 
 func main() {
-
-	a:= Person{"Alice"}
+	//shortand for type inference
+	//longhand for type declaration
 	//% gives a struct, 
 	//v is name of fields 
-	fmt.Printf("%+v \n", a)
+	fmt.Printf("%+v \n", alice)
 
 }
